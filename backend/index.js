58937cfe10e6dbcf7c4d1e3d4ec98b0cce4e6336
neysacalya <neysacalyaazarine@gmail.com>
2024-10-30 14:56:22 +0700

    suplier

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import bodyParser from "body-parser"
 import cors from  "cors"  
 import dotenv from "dotenv"
 import kasirRoute from "./routes/kasir.route.js"
+import suplierRoute from "./routes/suplier.route.js"
 
 const app = express()
 
@@ -13,6 +14,7 @@ app.use(bodyParser.json())
 app.use(cors())
 
 app.use("/kasir", kasirRoute)
+app.use("/suplier",suplierRoute)
 
 app.listen(process.env.app_port,() => {
     console.log("server run on "+ process.env.app_port)
---
+++ backend/routes/suplier.route.js
@@ -0,0 +1,21 @@
+import express from 'express';
+
+import {
+    getAllSuplier,
+    getSuplierById,
+    updateSuplier,
+    addSuplier,
+    deleteSuplier
+} from './controllers/suplier_controller.js';
+
+const app = express();
+app.use(express.json());
+
+app.get('/',getAllSuplier)
+app.get('/:id', getSuplierById)
+app.post('/', addSuplier)
+app.put('/:id', updateSuplier)
+app.delete('/:id', deleteSuplier)
+
+
+export default app
